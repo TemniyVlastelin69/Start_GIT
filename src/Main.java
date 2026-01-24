c7741cd482548983c24a11629fdc375f5335c16f
TemniyVlastelin69 <sktr@tut.by>
2026-01-24 20:58:41 +0300

    успешная синхронизация профиля гитхаб в IDE

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main (String[] args){
         //первое сообщение
         System.out.println ("Hello WORLD!!!");
+        System.out.println ("Success add gitHUB profile in this programm.");
     }
 
 }
