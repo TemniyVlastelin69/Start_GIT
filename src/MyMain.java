4c8e26870f7678b31c2eb95483b93394e7594499
TemniyVlastelin69 <sktr@tut.by>
2026-01-31 21:57:25 +0300

    первый калькулятор

---
+++ src/MyMain.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class MyMain {
+
+    public static void main(String[] args) {
+        System.out.println("***********************************************************************************");
+        System.out.println("***Добро пожаловать в базовый калькулятор для вычислений стандартных матопераций***");
+        System.out.println("-----------------------------------------------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        double res;
+
+        System.out.print("Укажите действие: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+
+        switch (action) {
+            case "+":
+                res = num1 + num2;
+                System.out.println("Результат сложения чисел: " + res);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Результат вычитания чисел: " + res);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Результат умножения чисел: " + res);
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("ERROR");
+                }
+                else {
+                    res = num1 / num2;
+                    System.out.println("Результат деления чисел: " + res);
+                }
+                break;
+            default:
+                System.out.println("Введены некорректные данные");
+        }
+    }
+}
